0e9ba6c4d45c6502c861f08d9ba3fbf13ee7630c
CMAC420 <cmac_13_@hotmail.com>
2026-01-07 21:38:17 +1100

    added admin metrix

--- main.go
+++ main.go
@@ -1,9 +1,59 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+type chirpRequest struct {
+	Body string `json:"body"`
+}
+
+type errorResponse struct {
+	Error string `json:"error"`
+}
+
+type validResponse struct {
+	Valid bool `json:"valid"`
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerAdminMetrics(w http.ResponseWriter, r *http.Request) {
+	hits := cfg.fileserverHits.Load()
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	html := fmt.Sprintf(`
+<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>
+	`, hits)
+	w.Write([]byte(html))
+}
+
+func (cfg *apiConfig) handlerAdminReset(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	cfg.fileserverHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write([]byte("Hits reset to 0"))
+}
 
 func readinessHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
@@ -11,16 +61,48 @@ func readinessHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("OK"))
 }
 
+func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader((http.StatusMethodNotAllowed))
+		return
+	}
+	decoder := json.NewDecoder(r.Body)
+	var req chirpRequest
+	if err := decoder.Decode(&req); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		resp, _ := json.Marshal(errorResponse{Error: "Invalid JSON"})
+		w.Header().Set("Content-Type", "application:json")
+		w.Write(resp)
+		return
+	}
+	if len(req.Body) > 140 {
+		w.WriteHeader(http.StatusBadRequest)
+		resp, _ := json.Marshal(errorResponse{Error: "Chirp is too long"})
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(resp)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	resp, _ := json.Marshal(validResponse{Valid: true})
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(resp)
+}
+
 func main() {
+	apiCfg := &apiConfig{}
 	const filepathRoot = "."
 	const port = "8080"
 
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/healthz", readinessHandler)
+	mux.HandleFunc("GET /api/healthz", readinessHandler)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerAdminMetrics)
+	mux.HandleFunc("POST /admin/reset", apiCfg.handlerAdminReset)
+	mux.HandleFunc("POST /api/validate_chirp", handlerValidateChirp)
+
 	fileserver := http.FileServer(http.Dir(filepathRoot))
 
-	mux.Handle("/app/", http.StripPrefix("/app", fileserver))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", fileserver)))
 
 	server := &http.Server{
 		Addr:    ":" + port,
